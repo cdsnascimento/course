b09abb8a55ae3eba3ffc003bc71ecdafab341ed6
Cristiano Nascimento <cs_nascimento@yahoo.com.br>
2020-11-02 17:48:34 -0300

    exibir nome de um arquivo
    Exibir apenas o nome de um determinado aquivo, a partir da informação do caminho completo pelo usuário

--- src/application/Program.java
+++ src/application/Program.java
@@ -9,30 +9,15 @@ public class Program {
 
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Enter a folder path: ");
+        System.out.println("Enter a file path: ");
         String strPath = sc.nextLine();
-
+        
         File path = new File(strPath);
-
-        //Listar todas as pastas a partir desta pasta
-        File[] folders = path.listFiles(File::isDirectory);
-
-        System.out.println("FOLDERS: ");
-        for (File folder : folders){
-            System.out.println(folder);
-        }
-
-        File[] files = path.listFiles(File::isFile);
-        System.out.println("FILES: ");
         
-        for (File file : files) {
-        	System.out.println(file);
-        }
+        //Exibindo apenas o nome do arquivo, sem as pastas;
+        System.out.println("getName: " + path.getName());
         
-        boolean success = new File(strPath + "\\subdir").mkdir();
         
-        System.out.println("Directory created successfully: " + success);
-
         sc.close();
 
     }
